bacee1c5d699588431bb4bf0c56b5e95b6ba532a
Felipe Martínez <40834220+luisfelipemo@users.noreply.github.com>
2020-11-16 15:37:53 -0600

    Estudios de gabinete
    Modificaciones en estudios de gabinete y formulario de buscador por fechas

--- resources/views/Admin/Citas/citas.blade.php
+++ resources/views/Admin/Citas/citas.blade.php
@@ -19,14 +19,14 @@
                             <div class="col-md-2">
                             <button type="button" style="margin: 24px -8px 0px;" class="form-control btn width-xs btn-primary">Buscar</button>
                             </div>
-                            <form action="{{route('citas.buscarFecha')}}" method="POST">
+                            <form class="row"action="{{route('citas.buscarFecha')}}" method="POST">
                             @csrf
                                 <div class="form-group mb-3">
                                                 <h4>Buscar por fechas</h4>
                                                 <input type="text" id="range-datepicker" name="Fechas" class="form-control col-lg-12" required placeholder="2018-10-03 a 2018-10-10">
                                 </div>
                                 <div class="form-group mb-3">
-                                    <button type="submit" style="margin: 35px 18px 0px;" class="form-control btn width-xs btn-primary">Buscar</button>
+                                    <button type="submit" style="margin: 39px 18px 0px;" class="form-control btn width-xs btn-primary">Buscar</button>
                                 </div>
                             </form>
                             </div>
--- resources/views/Admin/datosDeConsulta/estudiosGabinete.blade.php
+++ resources/views/Admin/datosDeConsulta/estudiosGabinete.blade.php
@@ -86,6 +86,22 @@
                 <input type="file" class="form-control" id="example-fileinput">
               </div>
             </div>
+            <div class="form-group row">
+              <label class="col-lg-2 col-form-label" for="example-fileinput">Nombre de tu foto</label>
+              <div class="col-lg-10">
+              <input type="text" class="form-control" id="validationCustom01" placeholder="Nombre de foto" value="" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-lg-2 col-form-label" for="example-fileinput">Descripción</label>
+                <div class="col-md-10">
+                  <textarea required class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="form-group col-md-12">
+              <a href="#" class="btn btn-danger"  data-dismiss="modal" >Cancelar</a>
+              <button class="btn btn-primary" type="submit">Subir foto</button>
+            </div>
           </div>
       </div>
       </form>
