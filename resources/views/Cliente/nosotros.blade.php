9fe9d3f5bf65345585598e7ac139f7dbba5797b1
Felipe Martínez <luisfelipemo1998@gmail.com>
2021-01-07 14:06:36 -0600

    CSS Cambios
    Modificaciones en el css

--- resources/views/Cliente/nosotros.blade.php
+++ resources/views/Cliente/nosotros.blade.php
@@ -92,7 +92,7 @@
               </div>
               <div class="form-group col-md-6">
                 <label for="inputPassword4">Ciudad</label>
-                <input type="password" class="form-control" id="inputPassword4" placeholder="Ej: Monterrey, N.L.">
+                <input type="text" class="form-control" id="inputPassword4" placeholder="Ej: Monterrey, N.L.">
               </div>
             </div>
             <div class="form-group">
